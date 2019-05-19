d97258f3eaa9aef94459de09648b4c9c91b553f4
Pickle Rick <dmryan711@gmail.com>
2019-05-19 09:02:27 -0400

    Set up mongoose and express server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const PORT = 3000;
 
 // // Require all models
-let db = require("./models");
+const db = require("./models");
 
 // Initialize Express
 const app = express();
@@ -20,10 +20,33 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/populate", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
+app.get("/",function(req,res){
+
+    db.articles.create({ 
+        headline: "headline 1",
+        summary:"Salt",
+        url: "Care"
+    
+    })
+    .then(function(article) {
+      // If saved successfully, print the new Library document to the console
+      console.log(article);
+      res.json({testDocCreated:"true"});
+    })
+    .catch(function(err) {
+      // If an error occurs, print it to the console
+      console.log(err.message);
+      res.json({testDocCreated:"false", error:err.message});
+    });  
+    
+    
+});
 // Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
